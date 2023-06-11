ec2f41356779545133a25b85483349fe2a4c5881
Erick <gets@cesar.school>
2023-06-11 13:13:02 -0300

    Estrutura montada do login

--- bibliotecaFready.h
+++ bibliotecaFready.h
@@ -80,55 +80,72 @@ void lg_residente (char email[20], lista_aluno* lista_de_alunos) {
         return;
     }
 
-    //Menu com:
-        //Dados do Residente
-        //Histórico de presença acessado pela lista de struct
-        // Menu do lg_residente
-    int opcao = -1;
-    while (opcao != 0) {
-        printf("\n--- Menu do Residente ---\n");
-        printf("1. Visualizar seus dados\n");
-        printf("2. Visualizar histórico de presença\n");
-        printf("0. Sair\n");
-        printf("Digite a opção desejada: ");
+    int paginaPrincipal = 1;
+    char senha_antiga[100],senha_nova[100],senha_Verificador[100];
+    int opcao,menu_navbar;
+    while (paginaPrincipal !=0){
+        printf("-- Pagina Principal --");
+        printf("\n\n");
+        //Printar tempo corrido @@G10@@
+        printf("Tempo decorrido:\n");
+        printf("\n\n");
+        printf("Acessar NavBar - 1 (Pagina Principal/Calendario/Menu)");
         scanf("%d", &opcao);
-
         switch(opcao) {
             case 1:
-                // Exibe os residente do lg_residente
-                printf("\n--- Dados do Residente ---\n");
-                printf("Nome: %s\n", Residente->aluno->nome);
-                printf("CPF: %s\n", Residente->aluno->cpf);
-                printf("E-mail: %s\n", Residente->aluno->email);
-                printf("E-mail do Preceptor: %s\n", Residente->aluno->email_preceptor);
-                printf("Especialização: %s\n", Residente->aluno->especializacao);
-                printf("ID do Preceptor: %s\n", Residente->aluno->email_preceptor);
-                printf("Histórico de presenças:\n");
+                break;
+            case 2:
+                // Exibe o histórico de presença do lg_residente
+                printf("Historico de presencas:\n");
+                int n=1;
                 while(Residente->aluno->lista_de_Presenca->next!=NULL){
-                    printf("Entrada: %ld\nSaida: %ld\n",Residente->aluno->lista_de_Presenca->entrada,Residente->aluno->lista_de_Presenca->saida);
-                    if (Residente->aluno->lista_de_Presenca->next!=NULL){
+                    printf("Presenca: %d\n\tEntrada: %ld\n\tSaida: %ld\n", n, Residente->aluno->lista_de_Presenca->entrada, Residente->aluno->lista_de_Presenca->saida);
+                    if (Residente->aluno->lista_de_Presenca->next!=NULL){ //problema por criar a lista antes de verificar se tem próximo
                     Residente->aluno->lista_de_Presenca=Residente->aluno->lista_de_Presenca->next;
                     }else {
                         Residente->aluno->lista_de_Presenca->next=NULL;
                     }
+                    n++;
                 }
+                printf("Aperter qualquer tecla para ir ao menu!");
+                getchar();
+                getchar();
                 break;
-            case 2:
-                // Exibe o histórico de presença do lg_residente
-                printf("\n--- Histórico de Presença ---\n");
-                //historico* temporario = Residente->Head;
-                // while (temporario != NULL) {
-                //     printf("Entrada: %s", asctime(localtime(&(temporario->entrada))));
-                //     printf("Saída: %s", asctime(localtime(&(temporario->saida))));
-                //     double diferenca = difftime(temporario->saida, temporario->entrada);
-                //     printf("Tempo de Trabalho: %lf segundos\n", diferenca);
-                //     printf("\n");
-                //     temporario = temporario->next;
-                // }
-                break;
-            case 0:
-                // Sair do menu
-                printf("\nSaindo...\nAte breve! By: Fready!\n");
+            case 3:
+                // Exibe os residente do lg_residente
+                printf("\n--- MENU ---\n\n");
+                printf("\n--- Dados do Residente ---\n");
+                printf("Logado: %s\n", Residente->aluno->nome);
+                printf("\nO que deseja fazer?\n(1)-Vizualizar Dados \t (2)-Trocar Senha \t (3)Sair\n");
+                scanf("%d", &menu_navbar);
+                if (menu_navbar==1){
+                    printf("\n--- Vizualizar Dados ---\n");   
+                    printf("Nome: %s\n", Residente->aluno->nome);
+                    printf("CPF: %s\n", Residente->aluno->cpf);
+                    printf("E-mail: %s\n", Residente->aluno->email);
+                    printf("E-mail do Preceptor: %s\n", Residente->aluno->email_preceptor);
+                    printf("Especializacao: %s\n", Residente->aluno->especializacao);
+                    printf("Aperter qualquer tecla para ir ao menu!");
+                    getchar();
+                    getchar();
+                }else if (menu_navbar==2) {
+                    printf("\n--- Trocar Senha ---\n");  
+                    printf("Digite a Senha Antiga:");
+                    fgets(senha_antiga,sizeof(senha_antiga),stdin);
+                    //condiçao se é verdadeira @@G10@@
+                    printf("Digite a Senha Nova:");
+                    fgets(senha_nova,sizeof(senha_nova),stdin);
+                    printf("Repita a senha Nova:");
+                    fgets(senha_Verificador,sizeof(senha_Verificador),stdin);
+                    //condição se as senhas novas batem
+                    if (strcmp(senha_nova,senha_Verificador)==0){
+                        //trocar senha no senhasBC.txt @@G10@@
+                        printf("Senha Trocada com Sucesso!\n");
+                        printf("Aperter qualquer tecla para ir ao menu!");
+                        getchar();
+                        getchar();
+                    }
+                }
                 break;
             default:
                 printf("\nOpção invalida, tente novamente.\n");
